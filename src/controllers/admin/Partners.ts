3d4497838bec28965749c373c48b58762d7ff277
gp-pereira <pereira7346@gmail.com>
2022-04-29 17:10:01 -0300

    feat: removed functionality

--- src/controllers/admin/Partners.ts
+++ src/controllers/admin/Partners.ts
@@ -1,14 +1,14 @@
-import { Get, Controller, Render, Post, Middleware, Redirect } from "routing-controllers";
+// import { Get, Controller, Render, Post, Middleware, Redirect } from "routing-controllers";
 
-@Controller("/admin/partners")
-export class PartnersController {
-	@Get("/")
-	@Render("create-partner-form")
-  read() { }
+// @Controller("/admin/partners")
+// export class PartnersController {
+// 	@Get("/")
+// 	@Render("create-partner-form")
+//   read() { }
   
-  @Post("/")
-  @Middleware(["admin"])
-  @Redirect("/admin/partners")
-  create() {}
-}
+//   @Post("/")
+//   @Middleware(["admin"])
+//   @Redirect("/admin/partners")
+//   create() {}
+// }
 
