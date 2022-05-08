119aa69b7cbe5715a814e913cfe2ab9cfd7e57aa
gp-pereira <pereira7346@gmail.com>
2022-05-08 12:58:26 -0300

    make it better

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ async function main(): Promise<void> {
 	const port = Number(process.env.PORT ?? 3001);
 
 	app.listen(port, () => {
-		console.log(`Server listening on port ${port}`);
+		console.log(`Server listening on port http://localhost:${port}`);
 	});
 }
 
