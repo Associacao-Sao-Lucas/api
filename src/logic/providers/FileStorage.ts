8c6707e329fdea6b47420e68b463ca77d8874308
gp-pereira <pereira7346@gmail.com>
2022-05-22 17:14:27 -0300

    feat: file storage interface

---
+++ src/logic/providers/FileStorage.ts
@@ -0,0 +1,13 @@
+export type InFile = {
+  originalname: string;
+  mimetype: string;
+  buffer: Buffer;
+}
+
+export type OutFile = {
+  id: string;
+}
+
+export interface FileStorage {
+  store(file: InFile): Promise<OutFile>;
+}
