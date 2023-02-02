985b0c63d678cfc3dabeea3fc8bf70ba23d5aaeb
gp-pereira <pereira7346@gmail.com>
2023-02-01 22:10:49 -0300

    feat: some tailwind tests
    Co-authored-by: jonascamargoo <jonascamargoo@users.noreply.github.com>

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/views/test.html'],
+  theme: {
+    colors: {
+      "header-border-mobile": "#060707",
+      "main-color": "#00a859",
+      "transition-color": "#337ab7",
+      "border-bottom-color": "#2a95be",
+      "background-gray-color": "#f5f5ff",
+      "gray-800": "#18272F",
+      "blue-header": "#051441",
+      "activities-blue": "#56607E"
+    },
+    fontFamily: {
+      'serif': 'europa'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
