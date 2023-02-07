c385e97e3716a32d48f319b350d6db426c003960
jonascamargoo <jonascamargo999@gmail.com>
2023-02-07 18:33:57 -0300

    feat: added home figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
       "main-color": "#00a859",
       "transition-color": "#337ab7",
       "border-bottom-color": "#2a95be",
+      "background-green-wave": "#74cda3",
       "background-gray-color": "#f5f5ff",
       "gray-800": "#18272F",
       "blue-header": "#051441",
       "main-gray": "#56607E",
-      "white": "#ffffff"
+      "white": "#ffffff",
+      "white-text": "#f6f6f6"
     },
     fontFamily: {
       'serif': 'europa'
